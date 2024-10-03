134d245e3302346b2a421630576526fbea9957b0
klassno3 <betelhemkirub@gmail.com>
2024-10-04 01:39:42 +0300

    added react scroll to footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Logo from "../../Image/logo.svg";
+import { Link } from "react-scroll";
 const Footer = () => {
   return (
     <div className="bg-black flex flex-col md:flex-row gap-10 justify-between items-start px-5 md:px-10 py-10 text-secondary-white-100/50">
@@ -9,10 +10,38 @@ const Footer = () => {
       </div>
       <div className="flex gap-[104px] md:gap-[168px]">
         <div className="flex flex-col gap-4">
-          <div className="cursor-pointer">Our Features</div>
-          <div className="cursor-pointer">Areas</div>
-          <div className="cursor-pointer">FAQs</div>
-          <div className="cursor-pointer">The Wait list</div>
+          <Link
+            to="features"
+            offset={-150}
+            smooth={true}
+            className="cursor-pointer hover:text-white/90"
+          >
+            Our Features
+          </Link>
+          <Link
+            to="areas"
+            offset={-150}
+            smooth={true}
+            className="cursor-pointer hover:text-white/90"
+          >
+            Areas
+          </Link>
+          <Link
+            to="faq"
+            offset={-150}
+            smooth={true}
+            className="cursor-pointer hover:text-white/90"
+          >
+            FAQs
+          </Link>
+          <Link
+            to="waitlist"
+            offset={-150}
+            smooth={true}
+            className="cursor-pointer hover:text-white/90"
+          >
+            The Wait list
+          </Link>
         </div>
         <div className="flex flex-col gap-4">
           <div className="cursor-pointer">Terms of service</div>
